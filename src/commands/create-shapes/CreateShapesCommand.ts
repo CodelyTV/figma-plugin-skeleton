1f89e993d54275606b8865fac03be5bc771fa8c5
Javier Ferrer González <javier.mailserio@gmail.com>
2022-08-03 14:47:27 +0200

    feat: add Plugin Menu commands and random fantasy* (#3)
    *Random fantasy:
    - Random shapes rotation
    - Random shapes creator command
    - Close plugin on error to avoid keeping the "Loading" on screen

--- src/commands/create-shapes/CreateShapesCommand.ts
+++ src/commands/create-shapes/CreateShapesCommand.ts
@@ -1,10 +1,15 @@
 import { Command } from "../../commands-setup/Command";
 
+export type SupportedShapes = "Rectangle" | "Ellipse";
+
 export class CreateShapesCommand implements Command {
   readonly type = "createShapes";
-  readonly payload: { numberOfShapes: number };
+  readonly payload: {
+    numberOfShapes: number;
+    typeOfShapes?: SupportedShapes;
+  };
 
-  constructor(numberOfShapes: number) {
-    this.payload = { numberOfShapes };
+  constructor(numberOfShapes: number, typeOfShapes?: SupportedShapes) {
+    this.payload = { numberOfShapes, typeOfShapes };
   }
 }
