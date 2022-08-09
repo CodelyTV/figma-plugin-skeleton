b4a56cc2208744f466ae481c439c8a391c768b97
Javier Ferrer <javier.mailserio@gmail.com>
2022-08-09 17:46:52 +0200

    test: implement `CancelCommandHandler` tests mocking out the Figma PluginAPI ✌️

--- src/scene-commands/cancel/CancelCommandHandler.ts
+++ src/scene-commands/cancel/CancelCommandHandler.ts
@@ -2,9 +2,11 @@ import { CommandHandler } from "../../commands-setup/CommandHandler";
 import { CancelCommand } from "./CancelCommand";
 
 export class CancelCommandHandler implements CommandHandler<CancelCommand> {
+  constructor(private readonly figma: PluginAPI) {}
+
   // `command` argument needed due to polymorphism.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   handle(command: CancelCommand): void {
-    figma.notify("👋  Good bye!");
+    this.figma.notify("👋  Good bye!");
   }
 }
