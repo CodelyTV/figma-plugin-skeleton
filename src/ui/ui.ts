fe29980f19414c4648b854754b34722f6cc70c96
Javier Ferrer <javier.mailserio@gmail.com>
2022-08-09 23:28:45 +0200

    feat: extract `handleCommand` to remove duplicity

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -1,9 +1,7 @@
 import "figma-plugin-ds/dist/figma-plugin-ds.css";
 import "./ui.css";
 
-import manifest from "../../manifest.json";
-import { Command } from "../commands-setup/Command";
-import { CommandsMapping } from "../commands-setup/CommandsMapping";
+import { handleCommand } from "../commands-setup/handleCommand";
 import { postMessage } from "../commands-setup/postMessage";
 import { CancelCommand } from "../scene-commands/cancel/CancelCommand";
 import { CreateShapesCommand } from "../scene-commands/create-shapes/CreateShapesCommand";
@@ -39,42 +37,4 @@ function registerUiCommandHandlers() {
 
     await handleCommand(command);
   };
-}
-
-async function handleCommand(command: Command) {
-  if (!(command.type in CommandsMapping)) {
-    notifyErrorToEndUser(
-      `Trying to execute the command \`${command.type}\` but it is not registered in the \`CommandsMapping.ts\` file. If you are the developer, go to the \`CommandsMapping.ts\` file and register it to the const with: \`${command.type}: ${command.type}CommandHandler,\``
-    );
-
-    figma.closePlugin();
-    return;
-  }
-
-  const commandHandler = CommandsMapping[command.type]();
-
-  try {
-    await commandHandler.handle(command);
-  } catch (error) {
-    notifyErrorToEndUser(
-      `"${error}" executing the command \`${command.type}\`. This command is mapped to a class in the \`CommandsMapping.ts\` file. It could be a good starting point to look for the bug 😊`
-    );
-  } finally {
-    const isACommandInsideAnotherCommand = command.type === "networkRequest";
-
-    if (!isACommandInsideAnotherCommand) {
-      figma.closePlugin();
-    }
-  }
-}
-
-function notifyErrorToEndUser(errorMessage: string): void {
-  figma.notify(
-    `🫣  Error in Figma plugin "${manifest.name}". See the JavaScript console for more info.`,
-    { error: true }
-  );
-
-  console.error(
-    `🫣️ Error in Figma plugin "${manifest.name}"\r\nFigma Plugin ID: "${figma.pluginId}"\r\n\r\n${errorMessage}.`
-  );
 }
