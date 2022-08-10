4a8b3a47daf21ad8976805d73a0abfda2f15ee8f
Javier Ferrer González <javier.mailserio@gmail.com>
2022-08-10 09:22:53 +0200

    feat: extract `handleCommand` to remove duplicity (#6)

---
+++ src/commands-setup/handleCommand.ts
@@ -0,0 +1,43 @@
+import manifest from "../../manifest.json";
+import { Command } from "./Command";
+import { CommandsMapping } from "./CommandsMapping";
+
+export async function handleCommand<CommandType extends Command>(
+  command: CommandType
+): Promise<void> {
+  if (!(command.type in CommandsMapping)) {
+    notifyErrorToEndUser(
+      `Trying to execute the command \`${command.type}\` but it is not registered in the \`CommandsMapping.ts\` file. If you are the developer, go to the \`CommandsMapping.ts\` file and register it to the const with: \`${command.type}: ${command.type}CommandHandler,\``
+    );
+
+    figma.closePlugin();
+    return;
+  }
+
+  const commandHandler = CommandsMapping[command.type]();
+
+  try {
+    await commandHandler.handle(command);
+  } catch (error) {
+    notifyErrorToEndUser(
+      `"${error}" executing the command \`${command.type}\`. This command is mapped to a class in the \`CommandsMapping.ts\` file. It could be a good starting point to look for the bug 😊`
+    );
+  } finally {
+    const isACommandInsideAnotherCommand = command.type === "networkRequest";
+
+    if (!isACommandInsideAnotherCommand) {
+      figma.closePlugin();
+    }
+  }
+}
+
+function notifyErrorToEndUser(errorMessage: string): void {
+  figma.notify(
+    `🫣  Error in Figma plugin "${manifest.name}". See the JavaScript console for more info.`,
+    { error: true }
+  );
+
+  console.error(
+    `🫣️ Error in Figma plugin "${manifest.name}"\r\nFigma Plugin ID: "${figma.pluginId}"\r\n\r\n${errorMessage}.`
+  );
+}
--- src/figma-entrypoint.ts
+++ src/figma-entrypoint.ts
@@ -1,6 +1,5 @@
-import manifest from "../manifest.json";
 import { Command } from "./commands-setup/Command";
-import { CommandsMapping } from "./commands-setup/CommandsMapping";
+import { handleCommand } from "./commands-setup/handleCommand";
 
 registerPluginMenuCommandHandlers();
 registerPluginMenuCommandParametersSuggestions();
@@ -47,42 +46,6 @@ function registerPluginUiCommandHandlers() {
   figma.ui.onmessage = async <CommandType extends Command>(
     command: CommandType
   ) => await handleCommand(command);
-}
-
-async function handleCommand<CommandType extends Command>(
-  command: CommandType
-): Promise<void> {
-  if (!(command.type in CommandsMapping)) {
-    notifyErrorToEndUser(
-      `Trying to execute the command \`${command.type}\` but it is not registered in the \`CommandsMapping.ts\` file. If you are the developer, go to the \`CommandsMapping.ts\` file and register it to the const with: \`${command.type}: ${command.type}CommandHandler,\``
-    );
-
-    figma.closePlugin();
-    return;
-  }
-
-  const commandHandler = CommandsMapping[command.type]();
-
-  try {
-    await commandHandler.handle(command);
-  } catch (error) {
-    notifyErrorToEndUser(
-      `"${error}" executing the command \`${command.type}\`. This command is mapped to a class in the \`CommandsMapping.ts\` file. It could be a good starting point to look for the bug 😊`
-    );
-  } finally {
-    figma.closePlugin();
-  }
-}
-
-function notifyErrorToEndUser(errorMessage: string): void {
-  figma.notify(
-    `🫣  Error in Figma plugin "${manifest.name}". See the JavaScript console for more info.`,
-    { error: true }
-  );
-
-  console.error(
-    `🫣️ Error in Figma plugin "${manifest.name}"\r\nFigma Plugin ID: "${figma.pluginId}"\r\n\r\n${errorMessage}.`
-  );
 }
 
 function createInvisibleUiForBrowserApiAccess() {
